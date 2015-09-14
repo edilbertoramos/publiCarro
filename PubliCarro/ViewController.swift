6de64f2ae5445f881ad22a1233d9c064d7195235
Edlberto Ramos <edilberto.ramos@icloud.com>
2015-09-14 11:08:34 -0400

    Adicionando o Parse ao projeto

--- PubliCarro/ViewController.swift
+++ PubliCarro/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
-
+import Parse
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    func Login(){
+        PFUser.logInWithUsernameInBackground("myUsername", password:"myPassword") {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                print("Login successful")
+                print("----\n%@", user?.email)
+            } else {
+                print("Login failed")
+            }
+        }
 
+    }
+    func SaveUser() {
+        let user = PFUser()
+        user.username = "myUsername"
+        user.password = "myPassword"
+        user.email = "email@example.com"
+        // other fields can be set just like with PFObject
+        user["phone"] = "415-392-0202"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                print(errorString)
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
